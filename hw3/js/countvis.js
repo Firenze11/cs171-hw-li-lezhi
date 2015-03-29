fda54525400d5951c993f905ddd3f009563b7283
Lezhi Li <lli2@gsd.harvard.edu>
2015-03-29 19:33:56 -0400

    power scale applied

--- hw3/js/countvis.js
+++ hw3/js/countvis.js
@@ -19,7 +19,7 @@ CountVis = function(_parentElement, _data, _metaData, _eventHandler){
     this.eventHandler = _eventHandler;
     this.displayData = [];
     // TODO: define all "constants" here
-    this.margin = {top: 20, right: 10, bottom: 30, left: 50},///////////////
+    this.margin = {top: 20, right: 10, bottom: 30, left: 80},///////////////
     this.width=750-this.margin.left-this.margin.right;//getHtWd(this.parentElement,"width")-this.margin.left-this.margin.right,
     this.height=350-this.margin.left-this.margin.right;//getHtWd(this.parentElement,"height")-this.margin.top-this.margin.bottom;
     this.initVis();
@@ -36,12 +36,12 @@ CountVis.prototype.initVis = function(){
     // --- ONLY FOR BONUS ---  implement zooming
 
     // TODO: modify this to append an svg element, not modify the current placeholder SVG element
-    this.svg = this.parentElement//.select("svg")
-                  .append("svg")
-                  .attr("width", this.width + this.margin.left + this.margin.right)
-                  .attr("height", this.height + this.margin.top + this.margin.bottom)
-                  .append("g")
-                  .attr("transform","translate("+this.margin.left+","+this.margin.top+")");
+    this.g = this.parentElement//this.svg is not an svg but a group
+                   .append("svg")
+                   .attr("width", this.width + this.margin.left + this.margin.right)
+                   .attr("height", this.height + this.margin.top + this.margin.bottom)
+                 .append("g")
+                   .attr("transform","translate("+this.margin.left+","+this.margin.top+")");
     // creates axis and scales
     this.x = d3.time.scale().range([0, this.width]);
     this.y = d3.scale.linear().range([this.height, 0]);
@@ -52,7 +52,7 @@ CountVis.prototype.initVis = function(){
                    .scale(this.y)
                    .orient("left");
     this.area = d3.svg.area()
-                      .interpolate("monotone")
+                      .interpolate("linear")//("monotone")
                       .x(function(d) { return that.x(d.time); })
                       .y0(this.height)
                       .y1(function(d) { return that.y(d.count); });
@@ -61,23 +61,24 @@ CountVis.prototype.initVis = function(){
                        .on("brush", function(){ console.log(that.brush.extent()); });
     // Trigger selectionChanged event. You'd need to account for filtering by time AND type
     // Add axes visual elements
-    this.svg.append("g")
-            .attr("class", "x axis")
-            .attr("transform", "translate(0," + this.height + ")")
-    this.svg.append("g")
-            .attr("class", "y axis")
-            .append("text")
-            .attr("transform", "rotate(-90)")
-            .attr("y", 6)   //??????????????????????????
-            .attr("dy", ".71em")
-            .style("text-anchor", "end")   //???????????????????????????
-            .text("Vote count, daily");
-    this.svg.append("g")
-            .attr("class", "brush");
+    this.g.append("g")
+          .attr("class", "x axis")
+          .attr("transform", "translate(0," + this.height + ")")
+    this.g.append("g")
+          .attr("class", "y axis")
+          .append("text")
+          .attr("transform", "rotate(-90)")
+          .attr("y", 6)   //??????????????????????????
+          .attr("dy", ".71em")
+          .style("text-anchor", "end")   //???????????????????????????
+          .text("Vote count, daily");
+    this.g.append("g")
+          .attr("class", "brush");
     //TODO: implement the slider -- see example at http://bl.ocks.org/mbostock/6452972
-    this.addSlider(this.svg)
+    //this.addSlider(this.parentElement.select("svg"));
     // filter, aggregate, modify data
     this.wrangleData();
+    this.addSlider(this.parentElement.select("svg"));
     // call the update method
     this.updateVis();
 }
@@ -93,12 +94,13 @@ CountVis.prototype.updateVis = function(){
     // TODO: implement update graphs (D3: update, enter, exit)
     // updates scales
     this.x.domain(d3.extent(this.displayData, function(d) { return d.time; }));
-    this.y.domain(d3.extent(this.displayData, function(d) { return d.count; }));
+    this.y.domain(d3.extent(this.displayData, function(d) { return d.count; })); 
     // updates axis
-    this.svg.select(".x.axis").call(this.xAxis);  //!!!!!!!!!!!!!!!!!!!!!!!!!!!call
-    this.svg.select(".y.axis").call(this.yAxis);
+    this.yAxis.scale(this.y);
+    this.g.select(".x.axis").call(this.xAxis);  
+    this.g.select(".y.axis").call(this.yAxis);
     // updates graph
-    var path = this.svg.selectAll(".area")
+    var path = this.g.selectAll(".area")
                        .data([this.displayData]);
     path.enter()
         .append("path")
@@ -108,10 +110,10 @@ CountVis.prototype.updateVis = function(){
     path.exit().remove();
 
     this.brush.x(this.x);
-    this.svg.select(".brush")
-            .call(this.brush)
-            .selectAll("rect")
-            .attr("height", this.height);
+    this.g.select(".brush")
+          .call(this.brush)
+          .selectAll("rect")
+          .attr("height", this.height);
 }
 /* Gets called by event handler and should create new aggregated data
  * aggregation is done by the function "aggregate(filter)". Filter has to
@@ -131,29 +133,37 @@ CountVis.prototype.onSelectionChange= function (selectionStart, selectionEnd){
 CountVis.prototype.addSlider = function(svg){
     var that = this;
     // TODO: Think of what is domain and what is range for the y axis slider !!
+    var data_extent = d3.extent(this.displayData,function(d){ return d.count; });
+    //var data_extent = [0,400];
     var sliderScale=d3.scale.linear()
-                            .domain(d3.extent(this.displayData,function(d){return d.count;}))
-                            .range([this.height,0]) ;
+                            .domain([0.2,1])
+                            .range([0,this.height]) ;
+                            //console.log(sliderScale.range());
     var sliderDragged = function(){
-        var value = Math.max(0, Math.min(200,d3.event.y));  /////////////////////200???
+        var value = Math.max(0, Math.min(that.height,d3.event.y));
         var sliderValue = sliderScale.invert(value);
+        //debugger;
         // TODO: do something here to deform the y scale
-        console.log("Y Axis Slider value: ", sliderValue);
+        //console.log("Y Axis Slider value: ", sliderValue);
         d3.select(this)
-          .attr("y", function () { return sliderScale(sliderValue); })
+          .transition()
+          .attr("y", function () { return value; });
+        that.y = d3.scale.pow()
+                         .exponent(sliderValue)
+                         .range([that.height, 0]); 
         that.updateVis({});
     }
     var sliderDragBehaviour = d3.behavior.drag().on("drag", sliderDragged)
     var sliderGroup = svg.append("g")
                          .attr({ class:"sliderGroup",
-                                 "transform":"translate("+0+","+30+")"  })
+                                 "transform":"translate("+0+","+20+")"  })
     sliderGroup.append("rect").attr({ class:"sliderBg",
                                       x:5,
                                       width:10,
-                                      height:200 })
+                                      height:that.height })
                               .style({ fill:"lightgray" })
     sliderGroup.append("rect").attr({"class":"sliderHandle",
-                                      y:0,
+                                      y:that.height,
                                       width:20,
                                       height:10,
                                       rx:2,
